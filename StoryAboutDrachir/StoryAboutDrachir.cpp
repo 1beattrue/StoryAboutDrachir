de0ca218b12a44e91e2a31e2e24e3fefbc0d586e
Vitaly <onebeattrue@gmail.com>
2022-11-09 18:20:11 +0300

    classMap

--- StoryAboutDrachir/StoryAboutDrachir.cpp
+++ StoryAboutDrachir/StoryAboutDrachir.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-
+#include <map>
 using namespace std;
 
 class Hero {
@@ -60,8 +60,59 @@ public:
 	}
 };
 
+class Map {
+private:
+	map <string, int> locations = { {"start", 0},  {"info", 1}, 
+									{"imp", 2},    {"elf", 3},
+									{"sphinx", 4}, {"traveler", 5},
+									{"demon", 6},  {"witch", 7}, 
+									{"boss", 8},   {"finish", 9} };
+public:
+	Map(string loc) {
+		
+	}
+
+	void moveToLocation(string loc) {
+		switch (locations[loc]) {
+		case 0:
+			// переход в локацию старт
+			break;
+		case 1:
+			// переход в локацию инфо
+			break;
+		case 2:
+			// переход в локацию импа
+			break;
+		case 3:
+			// переход в локацию эльфа
+			break;
+		case 4:
+			// переход в локацию сфинкса
+			break;
+		case 5:
+			// переход в локацию путника
+			break;
+		case 6:
+			// переход в локацию демона
+			break;
+		case 7:
+			// переход в локацию волшебницы
+			break;
+		case 8:
+			// переход в локацию сатаны
+			break;
+		case 9:
+			// переход в локацию финиш
+			break;
+		default:
+			cout << "Драчир: Не-е, туда я не пойду";
+			break;
+		}
+	}
+};
+
 
 int main()
 {
-	
+	Hero Drachir(3, 0, 0, 0, "start");
 }
